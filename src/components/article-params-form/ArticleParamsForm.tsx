c7539a9902e06e22a03a15093319a150a8ffdb9c
Ulyana Kulbida <unikub@yandex.ru>
2024-10-27 02:01:09 +0600

    Реализация формs из имеющихся компонентов согласно макету

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -4,9 +4,33 @@ import { Button } from 'src/ui/button';
 import clsx from 'clsx';
 import styles from './ArticleParamsForm.module.scss';
 import { useState } from 'react';
+import { Text } from 'src/ui/text';
+import { Select } from 'src/ui/select';
+import {
+	backgroundColors,
+	contentWidthArr,
+	defaultArticleState,
+	fontColors,
+	fontFamilyOptions,
+	fontSizeOptions,
+	OptionType,
+} from 'src/constants/articleProps';
+import { RadioGroup } from 'src/ui/radio-group';
+import { Separator } from 'src/ui/separator';
 
 export const ArticleParamsForm = () => {
 	const [isOpen, setIsOpen] = useState(false); // добавлено состояние с помощью хука
+	const [selectedFontFamily, setSelectedFontFamily] =
+		useState<OptionType | null>(defaultArticleState.fontFamilyOption);
+	const [selectedFontColors, setSelectedFontColors] =
+		useState<OptionType | null>(defaultArticleState.fontColor);
+	const [selectedBackgroundColors, setSelectedBackgroundColors] =
+		useState<OptionType | null>(defaultArticleState.backgroundColor);
+	const [selectedContentWidthArr, setSelectedContentWidthArr] =
+		useState<OptionType | null>(defaultArticleState.contentWidth);
+	const [selectedFontSize, setSelectedFontSize] = useState<OptionType>(
+		fontSizeOptions[0]
+	);
 
 	// обавлен обработчик для переключения состояния
 	const handleToggle = () => {
@@ -23,6 +47,41 @@ export const ArticleParamsForm = () => {
 					[styles.container_open]: isOpen,
 				})}>
 				<form className={styles.form}>
+					<Text as='h2' size={31} weight={800} uppercase dynamicLite>
+						Задайте параметры
+					</Text>
+					<Select
+						title='Шрифт'
+						options={fontFamilyOptions}
+						selected={selectedFontFamily}
+						onChange={setSelectedFontFamily}
+					/>
+					<RadioGroup
+						title='Размер шрифта'
+						name={RadioGroup.name}
+						options={fontSizeOptions}
+						selected={selectedFontSize}
+						onChange={setSelectedFontSize}
+					/>
+					<Select
+						title='Цвет шрифта'
+						options={fontColors}
+						selected={selectedFontColors}
+						onChange={setSelectedFontColors}
+					/>
+					<Separator />
+					<Select
+						title='Цвет фона'
+						options={backgroundColors}
+						selected={selectedBackgroundColors}
+						onChange={setSelectedBackgroundColors}
+					/>
+					<Select
+						title='Ширина контента'
+						options={contentWidthArr}
+						selected={selectedContentWidthArr}
+						onChange={setSelectedContentWidthArr}
+					/>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
 						<Button title='Применить' htmlType='submit' type='apply' />
